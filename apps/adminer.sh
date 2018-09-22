ad8144ed6b30f29d488a7fe008dbe90e3fe78d60
Daniel Hansson <github@hanssonit.se>
2018-09-22 15:13:50 +0200

    add adminer

---
+++ apps/adminer.sh
@@ -0,0 +1,120 @@
+
+#!/bin/bash
+
+# Tech and Me © - 2017, https://www.techandme.se/
+
+echo "Installing and securing Adminer..."
+
+# Prefer IPv4
+sed -i "s|#precedence ::ffff:0:0/96  100|precedence ::ffff:0:0/96  100|g" /etc/gai.conf
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/techandme/wordpress-vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Check if root
+if ! is_root
+then
+    printf "\n${Red}Sorry, you are not root.\n${Color_Off}You must type: ${Cyan}sudo ${Color_Off}bash %s/wordpress_install.sh\n" "$SCRIPTS"
+    exit 1
+fi
+
+# Check Ubuntu version
+if [ "$OS" != 1 ]
+then
+    echo "Ubuntu Server is required to run this script."
+    echo "Please install that distro and try again."
+    sleep 3
+    exit 1
+fi
+
+
+if ! version 16.04 "$DISTRO" 18.04.4; then
+    echo "Ubuntu version seems to be $DISTRO"
+    echo "It must be between 16.04 - 18.04.4"
+    echo "Please install that version and try again."
+    exit 1
+fi
+
+# Install Adminer
+apt update -q4 & spinner_loading
+install_if_not adminer
+sudo wget -q "http://www.adminer.org/latest.php" -O "$ADMINERDIR"/latest.php
+sudo ln -s "$ADMINERDIR"/latest.php "$ADMINERDIR"/adminer.php
+
+cat << ADMINER_CREATE > "$ADMINER_CONF"
+server {
+
+    # Listen on port 81
+    listen 81;
+
+    # Server name being used (exact name, wildcards or regular expression)
+    # server_name adminer.my;
+
+    root /usr/share/adminer/adminer;
+
+    # Logging
+    error_log /var/log/adminer/adminer.access_log;
+    access_log /var/log/adminer/adminer.error_log;
+
+
+   location / {
+           index  index.php;
+       }
+
+       ## Images and static content is treated different
+       location ~* ^.+.(jpg|jpeg|gif|css|png|js|ico|xml)$ {
+           access_log        off;
+           expires           360d;
+       }
+
+       location ~ /\.ht {
+           deny  all;
+       }
+
+       location ~ /(libraries|setup/frames|setup/libs) {
+           deny all;
+           return 404;
+}
+
+    # Pass the PHP scripts to FastCGI server
+    location ~ \.php$ {
+
+        fastcgi_pass unix:$PHP_FPM_SOCK;
+        fastcgi_split_path_info ^(.+\.php)(/.*)$;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME /usr/share/adminer/adminer$fastcgi_script_name;
+        fastcgi_param  HTTPS off;
+    }
+}
+ADMINER_CREATE
+
+# Enable config
+ln -s /etc/nginx/sites-available/adminer.conf /etc/nginx/sites-enabled/adminer.conf
+
+if ! service nginx restart
+then
+msg_box "Nginx could not restart...
+The script will exit."
+    exit 1
+else
+msg_box "Adminer was sucessfully installed and can be reached here:
+http://$ADDRESS/adminer.php
+
+You can download more plugins and get more information here: 
+https://www.adminer.org
+
+Your PostgreSQL connection information can be found in $NCPATH/config/config.php
+
+In case you try to access Adminer and get 'Forbidden' you need to change the IP in:
+$ADMINER_CONF"
+fi
+
+exit
