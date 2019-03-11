11e09c4ad1a49087ff2fcb77bb99789a9ce68ec1
Daniel Hansson <github@hanssonit.se>
2019-03-11 21:01:23 +0100

    Update techandme.sh

--- static/techandme.sh
+++ static/techandme.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-WANIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-ADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+
+# T&M Hansson IT AB © - 2019, https://www.hanssonit.se/
+
+WANIP6=$(curl -s -k -m 5 https://ipv6bot.whatismyipaddress.com)
+WANIP4=$(curl -s -k -m 5 https://ipv4bot.whatismyipaddress.com)
+ADDRESS=$(hostname -I | cut -d ' ' -f 1)
 WPADMINUSER=$(grep "WP USER:" /var/adminpass.txt)
-WPADMINPASS=$(grep "WP PASS:" /var/adminpass.txt)
+
 clear
 figlet -f small Wordpress
-echo "  https://www.hanssonit.se/wordpress-vm/"
+echo "https://www.hanssonit.se/wordpress-vm/"
 echo
-echo
-echo "|NETWORK|"
-echo "WAN IP: $WANIP"
-echo "LAN IP: $ADDRESS"
+echo "|Network|"
+echo "Hostname: $(hostname -s)"
+echo "WAN IPv4: $WANIP4"
+echo "WAN IPv6: $WANIP6"
+echo "LAN IPv4: $ADDRESS"
 echo
 echo "|WORDPRESS LOGIN|"
-echo "$WPADMINUSER"
-echo "$WPADMINPASS"
+echo "USER:$WPADMINUSER"
+echo "PASS:************"
 echo
-echo "|MySQL|"
+echo
+echo "|MySQL/MariaDB|"
+echo "USER: root"
 echo "PASS: cat /root/.my.cnf"
 echo
-exit 0
