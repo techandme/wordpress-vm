c22706ca1adabdea828146edd9aade65957d3d98
Daniel Hansson <github@hanssonit.se>
2019-03-12 21:07:40 +0100

    Update techandme.sh

--- static/techandme.sh
+++ static/techandme.sh
@@ -11,17 +11,17 @@ clear
 figlet -f small Wordpress
 echo "https://www.hanssonit.se/wordpress-vm/"
 echo
-echo "|Network|"
+echo "Network:"
 echo "Hostname: $(hostname -s)"
 echo "WAN IPv4: $WANIP4"
 echo "WAN IPv6: $WANIP6"
 echo "LAN IPv4: $ADDRESS"
 echo
-echo "|Wordpress Login|"
-echo "USER:$WPADMINUSER"
-echo "PASS: 'grep WP PASS: /var/adminpass.txt'"
+echo "Wordpress Login:"
+echo "$WPADMINUSER"
+echo "WP PASS: 'grep WP PASS: /var/adminpass.txt'"
 echo
-echo "|MySQL/MariaDB|"
+echo "MySQL/MariaDB:"
 echo "USER: root"
 echo "PASS: 'cat /root/.my.cnf'"
 echo
