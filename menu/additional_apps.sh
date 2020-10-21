d4f73b5609a27431ac73dc0c130437b17514e443
enoch85 <github@hanssonit.se>
2020-10-21 21:51:34 +0200

    add folders
    Signed-off-by: enoch85 <github@hanssonit.se>

---
+++ menu/additional_apps.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
+
+# shellcheck disable=2034,2059
+true
+SCRIPT_NAME="Additional Apps Menu"
+# shellcheck source=lib.sh
+source /var/scripts/fetch_lib.sh || source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Must be root
+root_check
+
+# Set the startup switch
+if [ -f "$SCRIPTS/nextcloud-startup-script.sh" ]
+then
+    STARTUP_SWITCH="ON"
+else
+    STARTUP_SWITCH="OFF"
+fi
+
+# Show a msg_box during the startup script
+if [ -f "$SCRIPTS/nextcloud-startup-script.sh" ]
+then
+    msg_box "In the next step, you will be offered to easily install apps that are made to enhance your server and experiance.
+We have pre-selected apps that we recommend for any installation.
+
+PLEASE NOTE: For stability reasons you should *not* select apps just for the sake of it.
+It's better to run: sudo bash $SCRIPTS/menu.sh when the first setup is complete, and after you've made a snapshot/backup of the server."
+fi
+
+# Install Apps
+choice=$(whiptail --title "$TITLE" --checklist \
+"Which apps do you want to install?\n\nAutomatically configure and install selected apps
+$CHECKLIST_GUIDE\n\n$RUN_LATER_GUIDE" "$WT_HEIGHT" "$WT_WIDTH" 4 \
+"Documentserver" "(OnlyOffice or Collabora - Docker or Integrated)" OFF \
+"Bitwarden" "(External password manager)" OFF \
+"Fail2ban  " "(Extra Bruteforce protection)" "$STARTUP_SWITCH" \
+"Adminer" "(PostgreSQL GUI)" OFF \
+"Netdata" "(Real-time server monitoring in Web GUI)" OFF \
+"BPYTOP" "(Real-time server monitoring in CLI)" OFF \
+"Midnight Commander" "(CLI file manager)" OFF \
+"FullTextSearch" "(Elasticsearch for Nextcloud [2GB RAM])" OFF \
+"PreviewGenerator" "(Pre-generate previews for Nextcloud)" "$STARTUP_SWITCH" \
+"LDAP" "(Windows Active directory for Nextcloud)" OFF \
+"Talk" "(Video calls and chat for Nextcloud)" OFF \
+"Webmin" "(Server GUI like Cpanel)" "$STARTUP_SWITCH" \
+"SMB-mount" "(Mount SMB-shares from your local network)" OFF 3>&1 1>&2 2>&3)
+
+case "$choice" in
+    *"Documentserver"*)
+        print_text_in_color "$ICyan" "Downloading the Documentserver Menu..."
+        run_script MENU documentserver
+    ;;&
+    *"Bitwarden"*)
+        print_text_in_color "$ICyan" "Downloading the Bitwarden Menu..."
+        run_script MENU bitwarden_menu
+    ;;&
+    *"Fail2ban  "*)
+        print_text_in_color "$ICyan" "Downloading the Fail2ban Menu..."
+        run_script MENU fail2ban_menu
+    ;;&
+    *"Adminer"*)
+        print_text_in_color "$ICyan" "Downloading the Adminer script..."
+        run_script APP adminer
+    ;;&
+    *"Netdata"*)
+        print_text_in_color "$ICyan" "Downloading the Netdata script..."
+        run_script APP netdata
+    ;;&
+    *"BPYTOP"*)
+        print_text_in_color "$ICyan" "Downloading the BPYTOP script..."
+        run_script APP bpytop
+    ;;&
+    *"Midnight Commander"*)
+        print_text_in_color "$ICyan" "Downloading the Midnight Commander script..."
+        run_script APP midnight-commander
+    ;;&
+    *"FullTextSearch"*)
+        print_text_in_color "$ICyan" "Downloading the FullTextSearch script..."
+        run_script APP fulltextsearch
+    ;;&
+    *"PreviewGenerator"*)
+        print_text_in_color "$ICyan" "Downloading the PreviewGenerator script..."
+        run_script APP previewgenerator
+    ;;&
+    *"LDAP"*)
+        SUBTITLE="LDAP"
+        print_text_in_color "$ICyan" "Installing LDAP..."
+        if install_and_enable_app user_ldap
+        then
+            msg_box "LDAP installed! Please visit https://subdomain.yourdomain.com/settings/admin/ldap \
+to finish the setup once this script is done." "$SUBTITLE"
+        else
+            msg_box "LDAP installation failed." "$SUBTITLE"
+        fi
+    ;;&
+    *"Talk"*)
+        print_text_in_color "$ICyan" "Downloading the Talk script..."
+        run_script APP talk
+    ;;&
+    *"Webmin"*)
+        print_text_in_color "$ICyan" "Downloading the Webmin script..."
+        run_script APP webmin
+    ;;&
+    *"SMB-mount"*)
+        print_text_in_color "$ICyan" "Downloading the SMB-mount script..."
+        run_script APP smbmount
+    ;;&
+    *)
+    ;;
+esac
+exit
---
+++ menu/server_configuration.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
+
+# shellcheck disable=2034,2059
+true
+SCRIPT_NAME="Server Configuration Menu"
+# shellcheck source=lib.sh
+source /var/scripts/fetch_lib.sh || source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Must be root
+root_check
+
+# Set the correct switch for activate_tls
+if [ -f $SCRIPTS/activate-tls.sh ]
+then
+    ACTIVATE_TLS_SWITCH="ON"
+else
+    ACTIVATE_TLS_SWITCH="OFF"
+fi
+
+# Set the startup switch
+if [ -f "$SCRIPTS/nextcloud-startup-script.sh" ]
+then
+    STARTUP_SWITCH="ON"
+else
+    STARTUP_SWITCH="OFF"
+fi
+
+# Show a msg_box during the startup script
+if [ -f "$SCRIPTS/nextcloud-startup-script.sh" ]
+then
+    msg_box "In the next step, you will be offered to easily install different configurations that are made to enhance your server and experiance.
+We have pre-selected some choices that we recommend for any installation.
+
+PLEASE NOTE: For stability reasons you should *not* select everything just for the sake of it.
+It's better to run: sudo bash $SCRIPTS/menu.sh when the first setup is complete, and after you've made a snapshot/backup of the server."
+fi
+
+# Server configurations
+choice=$(whiptail --title "$TITLE" --checklist \
+"Choose what you want to configure
+$CHECKLIST_GUIDE\n\n$RUN_LATER_GUIDE" "$WT_HEIGHT" "$WT_WIDTH" 4 \
+"Static IP" "(Set static IP in Ubuntu with netplan.io)" OFF \
+"Security" "(Add extra security based on this http://goo.gl/gEJHi7)" OFF \
+"DDclient Configuration" "(Use ddclient for automatic DDNS updates)" OFF \
+"Activate TLS" "(Enable HTTPS with Let's Encrypt)" "$ACTIVATE_TLS_SWITCH" \
+"GeoBlock" "(Only allow certain countries to access your server)" OFF \
+"Automatic updates" "(Automatically update your server every week on Sundays)" OFF \
+"SMTP Mail" "(Enable beeing notified by mail from your server)" OFF \
+"Disk Check" "(Check for S.M.A.R.T errors on your disks every week on Mondays)" OFF 3>&1 1>&2 2>&3)
+
+case "$choice" in
+    *"Static IP"*)
+        print_text_in_color "$ICyan" "Downloading the Static IP script..."
+        run_script NETWORK static_ip
+    ;;&
+    *"Security"*)
+        print_text_in_color "$ICyan" "Downloading the Security script..."
+        run_script ADDONS security
+    ;;&
+    *"DDclient Configuration"*)
+        print_text_in_color "$ICyan" "Downloading the DDclient Configuration script..."
+        run_script NETWORK ddclient-configuration
+    ;;&
+    *"Activate TLS"*)
+        SUBTITLE="Activate TLS"
+        msg_box "The following script will install a trusted
+TLS certificate through Let's Encrypt.
+It's recommended to use TLS (https) together with Nextcloud.
+Please open port 80 and 443 to this servers IP before you continue.
+More information can be found here:
+https://www.techandme.se/open-port-80-443/" "$SUBTITLE"
+
+        if yesno_box_yes "Do you want to install TLS?" "$SUBTITLE"
+        then
+            if [ -f $SCRIPTS/activate-tls.sh ]
+            then
+                bash $SCRIPTS/activate-tls.sh
+            else
+                print_text_in_color "$ICyan" "Downloading the Let's Encrypt script..."
+                download_script LETS_ENC activate-tls
+                bash $SCRIPTS/activate-tls.sh
+            fi
+        else
+            msg_box "OK, but if you want to run it later, just type: sudo bash $SCRIPTS/activate-tls.sh" "$SUBTITLE"
+        fi
+        
+        # Just make sure it is gone
+        rm -f "$SCRIPTS/test-new-config.sh"
+    ;;&
+    *"GeoBlock"*)
+        print_text_in_color "$ICyan" "Downloading the Geoblock script..."
+        run_script NETWORK geoblock 
+    ;;&
+    *"Automatic updates"*)
+        print_text_in_color "$ICyan" "Downloading the Automatic Updates script..."
+        run_script ADDONS automatic_updates
+    ;;&
+    *"SMTP Mail"*)
+        print_text_in_color "$ICyan" "Downloading the SMTP Mail script..."
+        run_script ADDONS smtp-mail
+    ;;&
+    *"Disk Check"*)
+        print_text_in_color "$ICyan" "Downloading the Disk Check script..."
+        run_script DISK smartctl
+    ;;&
+    *)
+    ;;
+esac
+exit
