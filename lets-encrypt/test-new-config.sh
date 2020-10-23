293c74d184df203ae2dbaa378f50f4011d38192a
Daniel Hansson <github@hanssonit.se>
2020-10-23 17:24:46 +0200

    Update test-new-config.sh

--- lets-encrypt/test-new-config.sh
+++ lets-encrypt/test-new-config.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # shellcheck disable=2034,2059
 true
+SCRIPT_NAME="Test New Configuration"
 # shellcheck source=lib.sh
-. <(curl -sL https://raw.githubusercontent.com/techandme/wordpress-vm/20.04_testing/lib.sh)
+source /var/scripts/fetch_lib.sh || source <(curl -sL https://raw.githubusercontent.com/techandme/wordpress-vm/20.04_testing/lib.sh)
 
-# T&M Hansson IT AB © - 2019, https://www.hanssonit.se/
+# T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
@@ -13,16 +14,15 @@ DEBUG=0
 debug_mode
 
 # Activate the new config
-printf "${Color_Off}We will now test that everything is OK\n"
-any_key "Press any key to continue... "
-ln -s /etc/nginx/sites-available/"$1" /etc/nginx/sites-enabled/"$1"
-rm -f /etc/nginx/sites-enabled/wordpress_port_80.conf
-rm -f /etc/nginx/sites-enabled/wordpress_port_443.conf
-rm -f /etc/nginx/sites-enabled/default.conf
-rm -f /etc/nginx/sites-enabled/default
+msg_box "We will now test that everything is OK"
+ln -s "$SITES_AVALIABLE"/"$1" "$SITES_ENABLED"/"$1"
+rm -f "$SITES_AVALIABLE"/"$HTTP_CONF"
+rm -f "$SITES_AVALIABLE"/"$TLS_CONF"
+rm -f "$NGINX_DEF"
+rm -f "$SITES_ENABLED"/default
 if restart_webserver
 then
-msg_box "New settings works! SSL is now activated and OK!
+    msg_box "New settings works! TLS is now activated and OK!
 
 This cert will expire in 90 days if you don't renew it.
 There are several ways of renewing this cert and here are some tips and tricks:
@@ -33,9 +33,9 @@ If you need to edit the crontab please type: crontab -u root -e
 If you need to edit the script itself, please check: $SCRIPTS/letsencryptrenew.sh
 
 Feel free to contribute to this project: https://goo.gl/3fQD65"
-    crontab -u root -l | { cat; echo "@daily $SCRIPTS/letsencryptrenew.sh"; } | crontab -u root -
+    crontab -u root -l | { cat; echo "3 */12 * * * $SCRIPTS/letsencryptrenew.sh"; } | crontab -u root -
 
-FQDOMAIN=$(grep -m 1 "server_name" "/etc/nginx/sites-enabled/$1" | awk '{print $2}')
+FQDOMAIN=$(grep -m 1 "server_name" "$SITES_ENABLED"/"$1" | awk '{print $2}')
 if [ "$(hostname)" != "$FQDOMAIN" ]
 then
     print_text_in_color "$ICyan" "Setting hostname to $FQDOMAIN..."
@@ -43,7 +43,7 @@ then
     # Change /etc/hosts as well
     sed -i "s|127.0.1.1.*|127.0.1.1       $FQDOMAIN $(hostname -s)|g" /etc/hosts
     # And in the php-fpm pool conf
-    sed -i "s|env\[HOSTNAME\] = .*|env[HOSTNAME] = $(hostname -f)|g" "$PHP_POOL_DIR/www_wordpress.conf"
+    sed -i "s|env\[HOSTNAME\] = .*|env[HOSTNAME] = $(hostname -f)|g" "$PHP_POOL_DIR"/wordpress.conf
 fi
 
 add_crontab_le() {
@@ -60,7 +60,7 @@ fi
 # Check if service is running
 if ! pgrep nginx > /dev/null
 then
-    service nginx start
+    start_if_stopped nginx.service
 fi
 CRONTAB
 }
@@ -71,14 +71,16 @@ chmod +x $SCRIPTS/letsencryptrenew.sh
 
 # Cleanup
 rm -f $SCRIPTS/test-new-config.sh
-rm -f $SCRIPTS/activate-ssl.sh
+rm -f $SCRIPTS/activate-tls.sh
 
 else
+
+
+
 # If it fails, revert changes back to normal
-    rm -f /etc/nginx/sites-enabled/"$1"
-    ln -s /etc/nginx/sites-available/wordpress_port_80.conf /etc/nginx/sites-enabled/ 
+    rm -f "$SITES_ENABLED"/"$1"
+    ln -s "$SITES_AVALIABLE"/"$HTTP_CONF" "$SITES_ENABLED"
     restart_webserver
-    printf "${ICyan}Couldn't load new config, reverted to old settings. Self-signed SSL is OK!${Color_Off}\n"
-    any_key "Press any key to continue... "
+    msg_box "Couldn't load new config, reverted to old settings. Self-signed TLS is OK!"
     exit 1
 fi
