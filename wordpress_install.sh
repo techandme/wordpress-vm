05c3c35ea5affce583a7d8e3d7f2491b7f5e3d6a
Daniel Hansson <mailto@danielhansson.nu>
2024-10-03 18:44:07 +0200

    install MariaDB rolling release

--- wordpress_install.sh
+++ wordpress_install.sh
@@ -213,13 +213,13 @@ done
 
 # Install MariDB repos
 install_if_not software-properties-common
-curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-11.4" --skip-maxscale
+curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash -s -- --skip-maxscale
 # Avoid i386 since we use x64
 sed -i "s|deb http|deb [arch=amd64] http|g" /etc/apt/sources.list.d/mariadb.list
 # USed debconf to install it
-sudo debconf-set-selections <<< "mariadb-server-11.4 mysql-server/root_password password $MARIADB_PASS"
-sudo debconf-set-selections <<< "mariadb-server-11.4 mysql-server/root_password_again password $MARIADB_PASS"
-install_if_not mariadb-server-11.4
+sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password password $MARIADB_PASS"
+sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password $MARIADB_PASS"
+install_if_not mariadb-server
 
 # mysql_secure_installation
 install_if_not expect
